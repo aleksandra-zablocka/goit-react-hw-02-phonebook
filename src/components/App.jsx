b659075e7d8062ff7c84255ad1c0468e031bb8dc
Aleksandra Zabłocka <aleksandra-zablocka@o2.pl>
2023-07-10 19:21:41 +0200

    added functionality that contact cant be repeated and updated some styles

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -8,20 +8,33 @@ class App extends Component {
     filter: '',
   };
 
-  // state = {
-  //   contacts: [ {id: 'id-1', name: 'Rosie Simpson', number: '459-12-56'},
-  //   {id: 'id-2', name: 'Hermione Kline', number: '443-89-12'},
-  //   {id: 'id-3', name: 'Eden Clements', number: '645-17-79'},
-  //   {id: 'id-4', name: 'Annie Copeland', number: '227-91-26'},],
-  //   filter: '',
-  //   name: '',
-  //   number: ''
-  // }
+  state = {
+    contacts: [
+      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+    ],
+    filter: '',
+    name: '',
+    number: '',
+  };
 
   addContact = newContact => {
-    this.setState(prevState => ({
-      contacts: [...prevState.contacts, newContact],
-    }));
+    const { contacts } = this.state;
+
+    const doesContactExist = contacts.some(
+      contact =>
+        contact.id === newContact.is || contact.name === newContact.name
+    );
+
+    if (doesContactExist) {
+      alert('There is such contact on the list');
+    } else {
+      this.setState(prevState => ({
+        contacts: [...prevState.contacts, newContact],
+      }));
+    }
   };
 
   deleteContact = id => {
